c9c9eccbbc0dbdd22d1934827de860de78c4dfbe
theplatypus <nicolas.bloyet@gmail.com>
2015-10-20 16:38:52 +0200

    Statistiques de MAJ
    Bonne ébauche de la fonction d’auto-ajustement des mises à jour

--- dev/controller/stats.js
+++ dev/controller/stats.js
@@ -13,27 +13,33 @@ function trace(msg){
 	if (verbose) console.log("[stats] :\t"+msg) ;
 }
 
-
 /*
  *	Affine la fréquence de mise à jour d'un flux
  */
 var studyFrequencyMAJ = function(url, stats){
 
-	/*var nbNew = stats.new.length,
+	var nbNew = stats.new.length,
 		nbTotal = nbNew + stats.old.length ;
 	id = md5(url);
 	trace("Studying "+url) ;
-	storer.get("MAJ", {_id : id}, {"frequency" : 1}, function(current){
-		trace(current) ;
-		if(current){ // cette url est deja connue
+	storer.get("MAJ", {_id : id}, {"lastEpoch" : 1 }, function(res){
+		console.log("Fréquence trouvée : ");
+		console.log(res);
+		if(res){ // cette url est deja connue
 
+			percentNew = ((nbNew)/(nbTotal))*100 ;
+			t = (new Date).getTime() - res.lastEpoch;
+			freq = (75*t)/percentNew ; // temps de renouvellement supposé pour 75%
+			storer.update("MAJ", {_id : id}, {lastEpoch : (new Date).getTime(), freq : freq}, function(res){ // on stocke le taux de renuovellement entre deux visites
+				trace(res) ;
+			}) ;
 
 		}else{	// cette url est nouvelle, on la stocke
-			storer.store("MAJ", {_id : id, "url" : url, "frequency" : 1}, function(res){ // on stocke le taux de renuovellement entre deux visites
-
-			})
+			storer.store("MAJ", {_id : id, "url" : url, lastEpoch : (new Date).getTime(), freq : 3600}, function(res){ // par défaut, on attend une heure
+				trace(res) ;
+			}) ;
 		}
-	}) ;*/
+	}) ;
 }
 
 exports.studyFrequencyMAJ = studyFrequencyMAJ ;
