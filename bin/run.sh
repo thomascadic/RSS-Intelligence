cde0a2f9bbed01aa6eb59ec2091e377dbbff764d
ThomasCadic <cadic.thomas@gmail.com>
2015-11-25 17:31:37 +0100

    run.sh update

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,13 @@
 ##
-##	demarre une instance de mongoDB qui stocke ses fichiers dans le dossier ../data/db
-##	puis demarre le serveur node.js
-mongod --dbpath ../data/db &
+## Demarre un replica-set MongoDB sur les ports 27017 et 27018, qui enregistre les données dans ../data
+## Demarre deux instances d'ElasticSearch 
+## Enfin, lance l'application
+
+/mongodb/mongodb-linux-x86_64-3.0.4/bin/mongod --port 27017 --dbpath ../data/db --replSet rs0 --smallfiles &
+/mongodb/mongodb-linux-x86_64-3.0.4/bin/mongod --port 27018 --dbpath ../data/db2 --replSet rs0 --smallfiles &
+
+../../ELK/elasticsearch-1.7.3-node-01/bin/elasticsearch &
+../../ELK/elasticsearch-1.7.3-node-02/bin/elasticsearch &
 
 wait 1
 
