e60c61bd4c42c037e3165718c2fbfa33da769d40
platypus <nicolas.bloyet@gmail.com>
2015-12-28 14:45:32 +0100

    MAJ dico

--- dev/stats/dico.js
+++ dev/stats/dico.js
@@ -1,5 +1,32 @@
-"use strict" // use of Map and Set (ES6 required)
-var Snowball = require('snowball'); 		// stemmer
+/**
+ *	Lit et met à jour deux fichiers dictionnaires
+ *	représentant chacun un corpus d'apprentissage d'une langue donnée.
+ *
+ *	Ce dictionnaire ne fait pas état de la correspondance terme <-> label
+ *	il rend compte de la répartition globale de ces termes sur l'ensemble du corpus
+ *	permettant par la suite d'étudier leur pertinence
+ *
+ * Le dictionnaire généré sera de la forme :
+ *	{
+ *		nbItems : INT 				// taille totale du corpus (nb de documents)
+ *		language : STRING
+ *		dico : MAP [
+ *			stem:STRING -> { 	id: INT,	// index du stem dans le dico
+ * 												tf: INT		// nombre de documents contenant stem
+ *												idf : INT // idf calculé lors de la création du dico
+ *																	// pour éviter trop de calculs par la suite
+ *																	// = log ( (nbItems) / (tf) )
+ *											}
+ *		]
+ *  }
+ */
+
+"use strict" // use of Set (ES6 required)
+
+var fs = require('fs');
+
+var tokenize = require('./tokenize').tokenize;
+
 var feedRead = require("feed-read") ;
 var htmlToText = require('html-to-text');
 
@@ -13,21 +40,47 @@ var reg_word = new RegExp(/[\s ,'"’`]+/g),
 	//reg_special_char = new RegExp(/[^\w\s]/gi);
 	reg_special_char = new RegExp(/[()“:"\r\n\\\/]+/gi);
 
-var dicoFR = new Map(),
-		dicoEN = new Map();
+var dicoFR = {},
+	dicoEN = {};
 
 var corpusFR = [],
-		corpusEN = [];
+	corpusEN = [];
 
-events.on('display_dico', function(language){
+var out = {},
+	corpus_out = [];
 
-			console.log("Dico "+language+" : ");
-			if (language == "french") console.log(dicoFR);
-			else console.log(dicoEN);
+events.on('display_dico', function(){
+			console.log(out);
+});
+
+events.on('write_dico', function(){
+
+	let path = "../../data/dico" + out.language + ".json" ;
+	fs.writeFile(path, JSON.stringify(out, null, "\t"), function (err) {
+		if (err) throw err;
+		console.log(path+" : saved");
+		});
+});
+
+/*
+ *	pour être sur de travailler exactement sur les memes données
+ *	lors des deux itérations, on sauvegarde le corpus sur disque
+ *	une mise à jour des feeds pourrait inférer sur les statistiques
+ *	Or, vu le nombre de feeds, l'hypothèse n'est pas improbable
+ */
+events.on('write_corpus', function(){
+
+	let path = "../../data/corpus.json";
+	fs.writeFile(path, JSON.stringify(corpus_out, null, "\t"), function (err) {
+		if (err) throw err;
+		console.log(path+" : saved");
+		});
 });
 
 /**
 *	Charge le contenu d'un fichier dans des corpus
+*	Tri sur language effectué en premier lieu,
+*	pour permettre à chaque dictionnaire de se constituer de façon asynchrone
 */
 var readFile = function(path){
 
@@ -53,9 +106,9 @@ var readFile = function(path){
 // etape 1 : générer un dictionnaire stem -> tf/idf
 var generateDictionnary = function(corpus, language, cb){
 
-	let stemmer = new Snowball(language);
 	let i = 0 ;
 	let todo = corpus.length ; // plutot utiliser Promise... mais pas trop de temps
+	let nbDocs = 0 ;	// nombre de documents étudiés
 
 	for(let feed of corpus){
 		//console.log(feed.url) ;
@@ -66,9 +119,10 @@ var generateDictionnary = function(corpus, language, cb){
 			}else{
 
 				for(let i in articles){
-
+					nbDocs++;
 					let article = articles[i];
-					let terms = new Set();
+					//let terms = new Set(); // contiendra les mots de l'item
+
 					// nettoyage item
 					article.content = htmlToText.fromString(article.content,{
 			    		wordwrap: false,
@@ -76,62 +130,54 @@ var generateDictionnary = function(corpus, language, cb){
 						ignoreHref: true
 					});
 
-					for(let term of article.content.split(reg_word)){
-						term = term.replace(reg_special_char, '');
-						stemmer.setCurrent(term);
-						stemmer.stem();
-						let stem = stemmer.getCurrent();
-						if(stem === "") break ;
-						else {
-							stem = stem.toLowerCase();
-							while(stem.endsWith(".") || stem.endsWith("…") || stem.endsWith(" ")){
-								stem = stem.substr(0, stem.length-1); // final point perturbant
-							}
-							terms.add(stem) ;
+					corpus_out.push({"label" : feed.category , "language" : language, "content" : article.content});
 
-						}
-						//console.log(language + " " + stem);
-					}
+					let terms = tokenize(article.content, language) ;
+					// pour la generation du dico, on ne note que la présence du terme dans le document
+					// -> on applique un filtre d'unicité au tableau obtenu
+					terms = terms.reverse().filter(function (e, i, arr) {
+					    return arr.indexOf(e, i+1) === -1;
+					}).reverse();
+
 					for (let stem of terms){
 						if(language === "french"){
-							if(dicoFR.has(stem)){
-								let val = dicoFR.get(stem) ;
-								val.tf += 1 ;
-								dicoFR.set(stem, val);
-							}else dicoFR.set(stem, {"id" : dicoFR.size  ,"tf" : 1}) ;
+							if(dicoFR[stem]){
+								dicoFR[stem].tf = dicoFR[stem].tf + 1;
+							}else dicoFR[stem] = {"id" : Object.keys(dicoFR).length  ,"tf" : 1} ;
 						}else{
-							if(dicoEN.has(stem)){
-								let val = dicoEN.get(stem) ;
-								val.tf += 1 ;
-								dicoEN.set(stem, val);
-							}else dicoEN.set(stem, {"id" : dicoEN.size  ,"tf" : 1}) ;
+							if(dicoEN[stem]){
+								dicoEN[stem].tf = dicoEN[stem].tf + 1;
+							}else dicoEN[stem] = {"id" : Object.keys(dicoEN).length  ,"tf" : 1} ;
 						}
 					}
 				}
 			}
 			todo-- ;
-			if(todo == 0){
+			if(todo == 0){ // called when all feeds have been treated
 				console.log("done") ;
-				dicoFR.forEach(function(value, key) {
-					value.idf = -1 * Math.log2(value.tf/dicoFR.size);
-				}, dicoFR);
-
-				dicoEN.forEach(function(value, key) {
-					value.idf = -1 * Math.log2(value.tf/dicoEN.size);
-				}, dicoEN);
-				events.emit('display_dico', language) ;
+				if(language === "french"){
+					for(let key in dicoFR){
+						// src wikipedia
+						//let term = dicoFR[key];
+						dicoFR[key].idf = Math.log( nbDocs / dicoFR[key].tf);
+					}
+					out.language = language;
+					out.nbItems = nbDocs ;
+					out.dico = dicoFR ;
+				}else{
+					for(let key in dicoEN){
+						dicoEN[key].idf = Math.log( nbDocs / dicoEN[key].tf);
+					}
+					out.language = language;
+					out.nbItems = nbDocs;
+					out.dico = dicoEN;
+				}
+				//events.emit('display_dico') ;
+				events.emit('write_dico');
+				events.emit('write_corpus');
 			}
 		});
 	}
-}
-
-// etape 2 : renseigner à chaque valeur sa catégorie
-// chaque feed génère un vecteur du type :
-// term1, term2, ..., term_n, LABEL
-// term étant des entrées du dictionnaire généré précédemment
-var generateVectors = function(key, category){
-
-
 }
 
 readFile("../../data/training.txt");
