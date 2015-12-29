ea912165d0949971205ff736442ab70297dc9560
platypus <nicolas.bloyet@gmail.com>
2015-12-29 14:23:50 +0100

    ADD naivebayes

---
+++ dev/stats/naivebayes.js
@@ -0,0 +1,12 @@
+/**
+*   NaiveBayes.js
+*   ==============
+*
+*   Implémentation d'un classifieur de type bayésien naïf multinomial
+*   cf. https://en.wikipedia.org/wiki/Naive_Bayes_classifier#Multinomial_naive_Bayes
+*
+*   Ce classifieur a pour but de calculer la probabilité d'appartenance à une classe
+*   d'un document, connaissant le tf-idf de chacun de ses termes le composant (features)
+*
+*   Pour ce faire, une base d'apprentissage a lieu à partir d'un corpus généré au préalable
+*/
